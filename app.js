a1886f6dabd96d7da2aae2bf5683c218c016beda
marvinhere <marvindevelop@gmail.com>
2025-05-23 22:27:34 -0600

    change to html

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const serverless = require('serverless-http');
-
-app.get('/', (req, res) => {
-  res.send('¡Hola desde Express en Vercel!');
-});
-
-module.exports.handler = serverless(app);
